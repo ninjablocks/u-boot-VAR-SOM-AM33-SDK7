ff2a200335b32295d98758c1f719769d702b1c6c
Jon Seymour <jon@ninjablocks.com>
2014-12-29 22:27:15 +1100

    Add crc32sum - a tool for calculating crc32 check sums in the same manner as u-boot.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- tools/crc32sum.c
+++ tools/crc32sum.c
@@ -29,5 +29,52 @@
 
 int main(int argc, char **argv)
 {
+	uint32_t crc;
+	const char *txt_filename = NULL;
+	int txt_fd;
+	struct stat txt_file_stat;
+	unsigned int filesize = 0;
+
+	unsigned char *filebuf = NULL;
+
+	int ret = EXIT_SUCCESS;
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: crc32sum filename\n");
+		return EXIT_FAILURE;
+	}
+
+	txt_filename = argv[1];
+	txt_fd = open(txt_filename, O_RDONLY);
+	if (txt_fd == -1) {
+		fprintf(stderr, "Can't open \"%s\": %s\n",
+				txt_filename, strerror(errno));
+		return EXIT_FAILURE;
+	}
+	/* ... and check it */
+	ret = fstat(txt_fd, &txt_file_stat);
+	if (ret == -1) {
+		fprintf(stderr, "Can't stat() on \"%s\": %s\n",
+				txt_filename, strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	filesize = txt_file_stat.st_size;
+
+	filebuf = mmap(NULL, sizeof(char) * filesize, PROT_READ,
+		       MAP_PRIVATE, txt_fd, 0);
+	if (filebuf == MAP_FAILED) {
+		fprintf(stderr, "mmap (%zu bytes) failed: %s\n",
+				sizeof(char) * filesize,
+				strerror(errno));
+		fprintf(stderr, "Falling back to read()\n");
+	}
+
+	crc = crc32(0, filebuf, filesize);
+
+	fprintf(stdout, "0x%08x\n", crc);
+
+	ret = close(txt_fd);
+
 	return 0;
 }
